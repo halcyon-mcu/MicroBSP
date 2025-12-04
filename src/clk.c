17359e722d46164ca87d588d3d3b0ac10e0eed1f
David Cruz <david@codebycruz.com>
2025-12-04 13:29:05 -0800

    Add pll info

--- src/clk.c
+++ src/clk.c
@@ -6,6 +6,11 @@ static clk_config_t globalConfig;
 // Info found on page 1520 of TRM.
 // SCICLK = (VCLK) / (P + 1 + (M/16))
 
+// Unverified:
+// f_INT_CLK = f_OSCIN / (REFCLKDIV + 1)
+// f_VCO_CLK = f_INT_CLK * ((PLLMUL / 256) + 1)
+// f_PLL_CLK = f_VCO_CLK / (PLLDIV + 1)
+
 void CLK_Init(clk_config_t config) {
 	globalConfig = config;
 }

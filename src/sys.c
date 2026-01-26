ad61ae60ec7464ab72f1b2a276c914fd5bbdc8a9
David Cruz <david@codebycruz.com>
2026-01-25 18:36:45 -0800

    feat(sys): memory initialization

--- src/sys.c
+++ src/sys.c
@@ -67,9 +67,66 @@ static void initClockMapping() {
 	sys1REG->GHVSRC = ((sys1REG->GHVSRC & ~(0b1111)) | CLKSRC_PLL1_OFFSET);
 }
 
+#define MINITGCR_ENABLE 0xAU
+#define MINITGCR_DISABLE 0x5U
+
+#define MSTCGSTAT_DONE (1 << 8)
+
+static void initMemoryUnit(uint32_t ram) {
+	sys1REG->MINITGCR = MINITGCR_ENABLE;
+
+	sys1REG->MSINENA = ram;
+
+	// Wait for memory init to complete
+	while ((sys1REG->MSTCGSTAT & MSTCGSTAT_DONE) != MSTCGSTAT_DONE) {
+		// Wait
+	}
+
+	sys1REG->MINITGCR = MINITGCR_DISABLE;
+}
+
+// RAM Peripherals can be found at page 116 of TRM
+// This also might help:
+// https://github.com/curtfs/CURTEV-19/blob/0422dc70b692c28fe645df4ce43fa2cfa3e9c7e0/EVMS_WS/source/sys_startup.c#L378
+#define MEMORY_UNIT_CPU (1 << 0)
+#define MEMORY_UNIT_MIBSPI5 (1 << 12)
+#define MEMORY_UNIT_MIBSPI3 (1 << 11)
+#define MEMORY_UNIT_MIBSPI1 (1 << 7)
+#define MEMORY_UNIT_DMA (1 << 1)
+#define MEMORY_UNIT_VIM (1 << 2)
+#define MEMORY_UNIT_MIBADC1 (1 << 8)
+#define MEMORY_UNIT_MIBADC2 (1 << 14)
+#define MEMORY_UNIT_DCAN1 (1 << 5)
+#define MEMORY_UNIT_DCAN2 (1 << 6)
+#define MEMORY_UNIT_DCAN3 (1 << 10)
+#define MEMORY_UNIT_NTHET1 (1 << 3)
+#define MEMORY_UNIT_NTHET2 (1 << 15)
+#define MEMORY_UNIT_HETTU1 (1 << 4)
+#define MEMORY_UNIT_HETTU2 (1 << 16)
+
+static void initMemory() {
+	initMemoryUnit(MEMORY_UNIT_CPU);
+
+	initMemoryUnit(MEMORY_UNIT_DMA | MEMORY_UNIT_VIM | MEMORY_UNIT_DCAN1 | MEMORY_UNIT_DCAN2 | MEMORY_UNIT_MIBADC1 |
+				   MEMORY_UNIT_NTHET1 | MEMORY_UNIT_HETTU1);
+}
+
+static void initRAM() {
+	// RAM doesn't need any wait states unlike flash.
+
+	// TODO: Handle any other necessary initialization for ram
+}
+
+static void initVIM() {
+	// TODO: This
+}
+
 void SYS_Init() {
 	initPLL();
 	initPeripherals();
 	FLASH_Init();
 	initClockMapping();
+	initMemory();
+	initRAM();
+	initVIM();
 }
